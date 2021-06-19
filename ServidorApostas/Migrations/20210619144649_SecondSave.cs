566ee286a26316bf41b7b93d0b19aa7226dd8125
João Costa <joao.pmc20@gmail.com>
2021-06-19 15:48:00 +0100

    new db migration

---
+++ ServidorApostas/Migrations/20210619144649_SecondSave.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServidorApostas.Migrations
+{
+    public partial class SecondSave : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
